025218e3ec1ddb52c8236a8351db9b997f49e278
Simon Walker <s.r.walker101@googlemail.com>
2023-02-01 06:20:15 +0000

    Remove flake utils and each system
    These are not required for templates

--- flake.nix
+++ flake.nix
@@ -1,35 +1,21 @@
 {
   description = "Flake utils demo";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [
-        ];
-
-        pkgs = import nixpkgs {
-          inherit overlays system;
+  outputs = { self, nixpkgs }:
+    {
+      templates = {
+        python = {
+          path = ./templates/python;
+          description = "Python template";
         };
-      in
-      {
-        templates = {
-          python = {
-            path = ./templates/python;
-            description = "Python template";
-          };
-          each-system = {
-            path = ./templates/each-system;
-            description = "Wrapper around numtide/flake-utils#each-system";
-          };
-          rust = {
-            path = ./templates/rust;
-            description = "Rust template";
-          };
+        each-system = {
+          path = ./templates/each-system;
+          description = "Wrapper around numtide/flake-utils#each-system";
         };
-      }
-    );
+        rust = {
+          path = ./templates/rust;
+          description = "Rust template";
+        };
+      };
+    };
 }
