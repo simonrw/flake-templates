7ac8259c666ddce632cd23d17ce2eb9ed45b3e62
Simon Walker <s.r.walker101@googlemail.com>
2023-02-17 11:41:33 +0000

    Update rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -2,13 +2,17 @@
   description = "Flake utils demo";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [
+          rust-overlay.overlays.default
         ];
 
         pkgs = import nixpkgs {
@@ -20,8 +24,7 @@
           default = rust-dev;
           rust-dev = pkgs.mkShell {
             buildInputs = with pkgs; [
-              rustc
-              cargo
+              rust-bin.beta.latest.default
               rust-analyzer
               clippy
               rustfmt
